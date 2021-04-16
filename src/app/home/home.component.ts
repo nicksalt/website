8549dfd26ee60aa9eecb000750fb944ca46a4dc1
Nick Salt <nicksalt24@gmail.com>
2021-04-15 20:38:29 -0400

    a few small updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,7 +34,6 @@ export class HomeComponent implements OnInit {
 
   ngAfterViewInit(){
     if(this.linkColor == null) {
-      this.linkColor = this.sassHelper.readProperty("secondary");
       console.log(this.linkColor);
       this.cdRef.detectChanges();
     }
